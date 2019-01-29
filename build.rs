431ac250e508eb6c433614fcd17d114fa82f014d
Manfred Endres <manfred.endres@tslarusso.de>
2019-01-29 11:20:51 +0100

    Add compatible version routes
    Description
    ===========
    
    This patch adds a set of new static routes to find the next compatible
    version based on the given version req and release type (final by
    default):
    
    ```
    version/2019 -> 404
    version/2019/alpha -> 2019.1.0a10
    ```

--- build.rs
+++ build.rs
@@ -1,12 +1,20 @@
+extern crate semver;
 extern crate serde;
-extern crate serde_yaml;
 extern crate serde_json;
+extern crate serde_yaml;
+extern crate uvm_core;
 
+use semver::VersionReq;
 use std::collections::BTreeMap;
+use std::collections::HashSet;
 use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
+use std::str::FromStr;
+use uvm_core::unity::fetch_matching_version;
+use uvm_core::unity::Version;
+use uvm_core::unity::VersionType;
 
 fn main() {
     let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
@@ -17,21 +25,116 @@ fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let routes_path = Path::new(&out_dir).join("version_routes.rs");
     let versions_path = Path::new(&out_dir).join("versions.rs");
+    let compatible_routes_path = Path::new(&out_dir).join("compatible_version_routes.rs");
 
     let mut f = File::create(&routes_path).unwrap();
-
-    writeln!(f, "pub fn _add_version_routes(router:&mut Router) {{");
-    writeln!(f, r#"    debug!("add unity version routes");"#);
-    writeln!(f, r#"    versions_routes!(router,"#);
-    for item in &version_list {
-        writeln!(f, r#"        "{}" => "{}","#, item.0, item.1);
-    }
-    writeln!(f, "    );");
-    writeln!(f, "}}");
+    write_versions_route(&mut f, &version_list);
 
     let mut f = File::create(&versions_path).unwrap();
+    write_version_content(&mut f, &version_list);
 
-    writeln!(f, "const VERSIONS: &str = r#\"");
-    writeln!(f, "{}", serde_json::to_string_pretty(&version_list).unwrap());
-    writeln!(f, "\"#;");
+    let mut f = File::create(&compatible_routes_path).unwrap();
+    write_compatible_routes(&mut f, &version_list);
+}
+
+fn write_versions_route(buffer: &mut File, version_list: &BTreeMap<String, String>) {
+    writeln!(buffer, "pub fn _add_version_routes(router:&mut Router) {{");
+    writeln!(buffer, r#"    debug!("add unity version routes");"#);
+    writeln!(buffer, r#"    versions_routes!(router,"#);
+    for item in version_list {
+        writeln!(buffer, r#"        "{}" => "{}","#, item.0, item.1);
+    }
+    writeln!(buffer, "    );");
+    writeln!(buffer, "}}");
+}
+
+fn write_version_content(buffer: &mut File, version_list: &BTreeMap<String, String>) {
+    writeln!(buffer, "const VERSIONS: &str = r#\"");
+    writeln!(
+        buffer,
+        "{}",
+        serde_json::to_string_pretty(version_list).unwrap()
+    );
+    writeln!(buffer, "\"#;");
+}
+
+fn write_compatible_routes<W: Write>(buffer: &mut W, version_list: &BTreeMap<String, String>) {
+    writeln!(
+        buffer,
+        "pub fn _add_compatible_version_routes(router:&mut Router) {{"
+    );
+
+    // convert versions to unity versions
+    let versions: Vec<Version> = version_list
+        .keys()
+        .map(|raw_version| Version::from_str(&raw_version).unwrap())
+        .collect();
+
+    // filter only major versions: 2018, 2019, etc
+    let major_versions: HashSet<u64> = versions.clone().into_iter().map(|v| v.major()).collect();
+    for major in major_versions {
+        for t in vec![
+            VersionType::Final,
+            VersionType::Patch,
+            VersionType::Beta,
+            VersionType::Alpha,
+        ] {
+            let req = VersionReq::from_str(&format!("~{}", major)).unwrap();
+            if let Ok(compatible_version) =
+                fetch_matching_version(versions.clone().into_iter(), req, t)
+            {
+                if t == VersionType::Final {
+                    writeln!(buffer, r#"        router.get("/version/{version}", |_r: &mut Request| version_result!("{compatible_version}"), "comp_version_{version}");"#, version = major,compatible_version = compatible_version);
+                }
+                writeln!(buffer, r#"        router.get("/version/{version}/{type:#}", |_r: &mut Request| version_result!("{compatible_version}"), "comp_version_{version}_{type}");"#, version = major, type = t,compatible_version = compatible_version);
+            }
+        }
+    }
+
+    // filter all major.minor combinations: 2018.1, 2018.2, etc
+    let major_minor_versions: HashSet<(u64, u64)> = versions
+        .clone()
+        .into_iter()
+        .map(|v| (v.major(), v.minor()))
+        .collect();
+    for major_minor in major_minor_versions {
+        for t in vec![
+            VersionType::Final,
+            VersionType::Patch,
+            VersionType::Beta,
+            VersionType::Alpha,
+        ] {
+            let req =
+                VersionReq::from_str(&format!("~{}.{}", major_minor.0, major_minor.1)).unwrap();
+            if let Ok(compatible_version) =
+                fetch_matching_version(versions.clone().into_iter(), req, t)
+            {
+                if t == VersionType::Final {
+                    writeln!(buffer, r#"        router.get("/version/{version}", |_r: &mut Request| version_result!("{compatible_version}"), "comp_version_{version}");"#, version = format!("{}.{}", major_minor.0, major_minor.1),compatible_version = compatible_version);
+                }
+                writeln!(buffer, r#"        router.get("/version/{version}/{type:#}", |_r: &mut Request| version_result!("{compatible_version}"), "comp_version_{version}_{type}");"#, version = format!("{}.{}", major_minor.0, major_minor.1), type = t,compatible_version = compatible_version);
+            }
+        }
+    }
+    // generate for all major.minor.patch combinations
+    for version in versions.clone() {
+        for t in vec![
+            VersionType::Final,
+            VersionType::Patch,
+            VersionType::Beta,
+            VersionType::Alpha,
+        ] {
+            let req = VersionReq::from_str(&format!("~{}", version.base())).unwrap();
+            if let Ok(compatible_version) =
+                fetch_matching_version(versions.clone().into_iter(), req, t)
+            {
+                if t == VersionType::Final {
+                    writeln!(buffer, r#"        router.get("/version/{version}", |_r: &mut Request| version_result!("{compatible_version}"), "comp_version_{version}");"#, version = version.base(),compatible_version = compatible_version);
+                }
+                writeln!(buffer, r#"        router.get("/version/{version}/{type:#}", |_r: &mut Request| version_result!("{compatible_version}"), "comp_version_{version}_{type}");"#, version = version.base(), type = t,compatible_version = compatible_version);
+            }
+        }
+    }
+
+    writeln!(buffer, "}}");
 }
